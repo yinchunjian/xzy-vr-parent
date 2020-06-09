54cc6a3d9ecbc7c06ae51d0dd93cd2d27b5103c0
yinchunjian <yinchunjian@163.com>
2020-06-09 10:51:29 +0800

    项目创建

--- vr-base-common/src/main/java/com/xzy/vr/common/constant/CommonConstant.java
+++ vr-base-common/src/main/java/com/xzy/vr/common/constant/CommonConstant.java
@@ -72,11 +72,19 @@ public interface CommonConstant {
     public static final Integer SC_JEECG_NO_AUTHZ=510;
 
     /** 登录用户Shiro权限缓存KEY前缀 */
-    public static String PREFIX_USER_SHIRO_CACHE  = "shiro:cache:org.jeecg.modules.shiro.authc.ShiroRealm.authorizationCache:";
+    public static String PREFIX_USER_SHIRO_CACHE  = "shiro:cache:com.xzy.boss.modules.shiro.authc.ShiroRealm.authorizationCache:";
     /** 登录用户Token令牌缓存KEY前缀 */
     public static final String PREFIX_USER_TOKEN  = "prefix_user_token_";
     /** Token缓存时间：3600秒即一小时 */
     public static final int  TOKEN_EXPIRE_TIME  = 3600;
+
+
+    /** APP登录用户Shiro权限缓存KEY前缀 */
+    public static String APP_PREFIX_USER_SHIRO_CACHE  = "shiro:cache:com.xzy.app.modules.shiro.authc.ShiroRealm.authorizationCache:";
+    /** APP登录用户Token令牌缓存KEY前缀 */
+    public static final String APP_PREFIX_USER_TOKEN  = "app_prefix_user_token_";
+    /** APPToken缓存时间：3600秒即一小时 */
+    public static final int  APP_TOKEN_EXPIRE_TIME  = 3600;
     
 
     /**
